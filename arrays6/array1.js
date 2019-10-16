9db3c0b7b698132bc849de3a45f22aa591a70db6
Akonya John <john.akonya@gmail.com>
2019-10-16 10:31:14 +0300

    made changes to folders

---
+++ arrays6/array1.js
@@ -0,0 +1,8 @@
+/*  arrays */
+
+var evenNum = [ ]  // array with zero variables
+
+var evenNum = [2 ,5 ,6]
+
+evenNum = n - 1  // <------- size od an array
+/* for instance  var evenNum has 3 - 1 = 2  , so 2 is assigned 0, 5 is assignend 1, and 6 is assigned 2 */
---
+++ conditions5/loops1.js
@@ -0,0 +1,28 @@
+/* find out the difference between while and for loop */
+
+/* while loops*/
+
+var numValue = 0
+while (numValue <= 10) {
+    console.log(numValue)
+    numValue++
+}
+console.log('\n')
+
+/* find out the difference
+
+/* for loop */
+for (var givenNum = 0; givenNum <= 10; givenNum++) {
+    console.log(givenNum)
+}
+console.log('\n')
+
+/* do loop */
+var someNum = 0
+do {
+    console.log(someNum)
+    someNum++
+} while (someNum <= 10) {
+    console.log(someNum)
+    someNum++
+}
